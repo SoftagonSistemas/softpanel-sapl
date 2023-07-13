af88a5f69196a6a92b1203837ecfd7fefc265735
Pedro-HenriqueDev <ph1305803@gmail.com>
2023-07-13 14:18:57 -0300

    remove/vitepress

--- apps/docs/pages/.vitepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import MyButton from '@myturborepo/ui/components/Button.vue'
-
-export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('MyButton', MyButton)
-  },
-}
